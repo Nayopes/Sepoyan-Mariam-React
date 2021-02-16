2cb23570c30800402da1f5b1cb519ad01a38a390
Nayopes <mariam.sepoyan13@gmail.com>
2021-02-16 23:40:32 +0400

    Remaking lesson8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
-// import Grid from './Components/ToDo/Grid'
 import ToDo from './Components/ToDo/ToDo'
 
 function App() {
   return (
     <div className="App">
-      {/* <Grid /> */}
       <ToDo />
     </div>
   )
