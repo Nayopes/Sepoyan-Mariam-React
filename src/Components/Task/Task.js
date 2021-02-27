c34bf94a4670a97cbaa8382908a2206945927d1b
Nayopes <mariam.sepoyan13@gmail.com>
2021-02-27 18:31:26 +0400

    delete my comments for clean work

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//import React, {memo} from 'react'
 import {Card, Button} from 'react-bootstrap'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faTrash , faEdit} from '@fortawesome/free-solid-svg-icons'
@@ -36,29 +35,4 @@ class Task extends React.PureComponent{
 
 export default Task
 
-// const Task = ({task, handleDeleteOneTask, toggleSetRemoveTaskIds, checked}) => {
-//     return(
-//         <Card className={`${styles.card} ${checked && styles.checked}`}>
-//             <Card.Body>
-//                 <input 
-//                 type = "checkbox"
-                
-//                 onClick = {()=>toggleSetRemoveTaskIds(task._id)}
-//                 />
-//                 <Card.Title> {task.title} </Card.Title>
-//                 <Card.Text> {task.description} </Card.Text>
-//                 <Button 
-//                     variant="danger"
-//                     onClick = {() => handleDeleteOneTask(task._id)}
-//                 >
-//                     <FontAwesomeIcon icon={faTrash} />
-//                 </Button>
-//                 <Button variant="warning" className="ml-3">
-//                     <FontAwesomeIcon icon={faEdit} />
-//                 </Button>
-//             </Card.Body>
-//         </Card>
-//     )
-// }
-// export default memo(Task)
 
